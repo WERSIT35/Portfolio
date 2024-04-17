44a799b1d400cfa3502cb96573516b441dee268c
Oto <okupara123@gmail.com>
2024-04-17 20:58:04 +0400

    pruw

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import Aos from 'aos';
 import { filter } from 'rxjs';
@@ -14,9 +14,28 @@ import { filter } from 'rxjs';
 export class HeaderComponent implements OnInit{
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  isMenuOpen: boolean = false;
+
+  toggleNav() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       Aos.init();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    // Check if the clicked element is outside the menu and the hamburger icon
+    const menu = document.querySelector('.menubar');
+    const hamburger = document.querySelector('.hamburger');
+    if (!menu?.contains(event.target as Node) && !hamburger?.contains(event.target as Node)) {
+      this.isMenuOpen = false;
     }
   }
 }
